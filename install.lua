4750d925a4f992f0bc1ac652332c3632c8b7959e
calebic <559caleb@gmail.com>
2026-01-28 23:51:41 -0600

    Update install.lua

--- install.lua
+++ install.lua
@@ -2,14 +2,102 @@
 
 term.setPaletteColour(colours.black, 0x000F1C)
 term.setPaletteColour(colours.lime, 0x0DDA85)
+term.setPaletteColour(colours.green, 0x0AA86A)
+term.setPaletteColour(colours.cyan, 0x39E6D3)
+term.setPaletteColour(colours.lightGrey, 0x8FD9C8)
+term.setPaletteColour(colours.orange, 0xFFB347)
+term.setPaletteColour(colours.red, 0xFF4C4C)
 term.setBackgroundColor(colours.black)
 term.setTextColour(colours.lime)
 shell.run("clear")
 
-local function printCentered(text)
+local function printCentered(text, y)
   local w, _ = term.getSize()
-  term.setCursorPos(math.floor((w - #text) / 2) + 1, select(2, term.getCursorPos()))
-  print(text)
+  local x = math.floor((w - #text) / 2) + 1
+  if y then term.setCursorPos(x, y) else term.setCursorPos(x, select(2, term.getCursorPos())) end
+  write(text)
+end
+
+local function drawFrame()
+  local w, h = term.getSize()
+  term.setBackgroundColor(colours.black)
+  term.setTextColour(colours.green)
+  term.setCursorPos(1, 1)
+  write(string.rep("=", w))
+  term.setCursorPos(1, 2)
+  write(string.rep("-", w))
+  term.setCursorPos(1, h - 1)
+  write(string.rep("-", w))
+  term.setCursorPos(1, h)
+  write(string.rep("=", w))
+end
+
+local function drawHeader()
+  local w, _ = term.getSize()
+  term.setTextColour(colours.cyan)
+  printCentered("ICYTEK INSTALLATION CONSOLE", 1)
+  term.setTextColour(colours.lightGrey)
+  local tag = "TEK-OS DISTRIBUTION NODE"
+  term.setCursorPos(w - #tag + 1, 2)
+  write(tag)
+end
+
+local function drawFooter(text)
+  local w, h = term.getSize()
+  term.setTextColour(colours.lightGrey)
+  term.setCursorPos(2, h - 1)
+  term.clearLine()
+  write(text or "W/S MOVE  ENTER SELECT  Q EXIT")
+  term.setTextColour(colours.green)
+  term.setCursorPos(2, h)
+  term.clearLine()
+  write("READY >")
+end
+
+local function drawScanlines()
+  local w, h = term.getSize()
+  term.setTextColour(colours.green)
+  for y = 4, h - 2, 2 do
+    term.setCursorPos(1, y)
+    write(string.rep(".", w))
+  end
+end
+
+local function bootSplash()
+  local w, h = term.getSize()
+  term.clear()
+  drawFrame()
+  term.setTextColour(colours.cyan)
+  printCentered("TEK-OS VAULTLINK BOOT", 4)
+  term.setTextColour(colours.lightGrey)
+  printCentered("ESTABLISHING SECURE LINK...", 6)
+  term.setTextColour(colours.lime)
+  for i = 1, 3 do
+    term.setCursorPos(2, 8 + i)
+    write("SYSCHK-" .. i .. " OK")
+    sleep(0.08)
+  end
+  term.setTextColour(colours.orange)
+  printCentered("LOADING MODULES", 12)
+  for i = 1, w - 6 do
+    term.setCursorPos(3, 14)
+    write(string.rep("=", i))
+    sleep(0.01)
+  end
+  term.setTextColour(colours.lime)
+  printCentered("READY", h - 3)
+  sleep(0.4)
+end
+
+local function flicker()
+  local w, h = term.getSize()
+  for i = 1, 8 do
+    local y = math.random(4, h - 2)
+    local x = math.random(1, w - 3)
+    term.setTextColour(colours.green)
+    term.setCursorPos(x, y)
+    write("..")
+  end
 end
 
 local function promptLine(label, default)
@@ -46,28 +134,35 @@ end
 
 local function drawMenu(options, selected)
   term.clear()
-  term.setCursorPos(1, 2)
-  printCentered("ICEYTEK INSTALLER")
-  printCentered("TEK-OS DISTRIBUTION")
-  print("")
+  drawFrame()
+  drawHeader()
+  drawScanlines()
+
+  local w, h = term.getSize()
+  term.setTextColour(colours.orange)
+  printCentered("VAULTLINK INSTALLER v76.3", 3)
+
+  local startY = 6
+  local boxWidth = math.min(w - 8, 34)
+  local boxX = math.floor((w - boxWidth) / 2) + 1
+
   for i, opt in ipairs(options) do
+    local y = startY + (i - 1) * 2
+    term.setCursorPos(boxX, y)
     if i == selected then
       term.setBackgroundColor(colours.lime)
       term.setTextColour(colours.black)
+      write("> " .. opt .. string.rep(" ", boxWidth - #opt - 3))
     else
       term.setBackgroundColor(colours.black)
       term.setTextColour(colours.lime)
+      write("  " .. opt .. string.rep(" ", boxWidth - #opt - 2))
     end
-    term.setCursorPos(4, 4 + i)
-    write(opt)
   end
+
   term.setBackgroundColor(colours.black)
   term.setTextColour(colours.lime)
-
-  local w, h = term.getSize()
-  term.setCursorPos(1, h)
-  term.clearLine()
-  write("W/S: Move  ENTER: Select")
+  drawFooter("W/S MOVE  ENTER SELECT  Q EXIT")
 end
 
 local MAINFRAME_URL = "https://raw.githubusercontent.com/calebic/icytek/refs/heads/main/Mainframe/startup.lua"
@@ -150,8 +245,10 @@ local options = {
 }
 
 local selected = 1
+bootSplash()
 while true do
   drawMenu(options, selected)
+  flicker()
   local _, key = os.pullEvent("key")
   if key == keys.w then
     selected = selected - 1
@@ -159,6 +256,9 @@ while true do
   elseif key == keys.s then
     selected = selected + 1
     if selected > #options then selected = 1 end
+  elseif key == keys.q then
+    term.clear()
+    return
   elseif key == keys.enter or key == keys.numPadEnter then
     if options[selected] == "Exit" then
       term.clear()
